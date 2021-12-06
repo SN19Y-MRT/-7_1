da1006f46a5205f232fd53e81a7f37d1480f4e5e
SN19Y-MRT <yuuki.karate1ban@icloud.com>
2021-12-06 13:43:06 +0900

    ブログ詳細画面表示

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,17 +2,19 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
+        <title>Blog</title>
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
-    
-    <h2 class='title'>
-    <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-    </h2>
-
+    <body>
+        <h1>Blog Name</h1>
+        <div class='posts'>
+            @foreach ($posts as $post)
+                <div class='post'>
+                    <h2 class='title'><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h2>
+                    <p class='body'>{{ $post->body }}</p>
+                </div>
+            @endforeach
+        </div>
+    </body>
 </html>
