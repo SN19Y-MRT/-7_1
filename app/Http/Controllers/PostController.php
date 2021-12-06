33e57879a270d74ebdada96388e1a72bb8005497
SN19Y-MRT <yuuki.karate1ban@icloud.com>
2021-12-06 14:35:28 +0900

    ブログ詳細画面表示4

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,18 +5,24 @@ namespace App\Http\Controllers;
 use App\Post;
 use Illuminate\Http\Request;
 
-
 class PostController extends Controller
 {
-    public function show(Post $post)
-        {
-            /**
-             * 特定IDのpostを表示する
-             *
-             * @params Object Post // 引数の$postはid=1のPostインスタンス
-             * @return Reposnse post view
-             */
-            return view('show')->with(['post' => $post]);
-        }
+    public function index(Post $post)
+{
+    return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
+}
+
+/**
+ * 特定IDのpostを表示する
+ *
+ * @params Object Post // 引数の$postはid=1のPostインスタンス
+ * @return Reposnse post view
+ */
+public function show(Post $post)
+{
+    return view('views/show')->with(['post' => $post]);
+}
 
 }
+
+?>
