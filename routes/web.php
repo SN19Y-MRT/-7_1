0d194703375cf6f9444f1fccdc34460057568a7b
SN19Y-MRT <yuuki.karate1ban@icloud.com>
2021-12-23 14:42:05 +0900

    ブランチ切り替え3

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,10 @@
 */
 Route::get('/', 'PostController@index');
 Route::get('/posts/create', 'PostController@create');
+Route::post('/posts', 'PostController@store');
 Route::get('/posts/{post}', 'PostController@show');
 
-Route::post('/posts', 'PostController@store');
+
 
 Route::get('/posts/{post}/edit', 'PostController@edit');
 Route::put('/posts/{post}', 'PostController@update');
